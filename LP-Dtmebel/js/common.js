396e151a9bd1847e1fbcdc0cd1248d7827acf670
John Polukhin <evgeniy07091989@gmail.com>
2017-06-18 18:01:53 +0300

    obnova

--- LP-Dtmebel/js/common.js
+++ LP-Dtmebel/js/common.js
@@ -89,21 +89,40 @@ $(function() {
 		offset: 85
 	});
 
-	$(".image-popup-tizer").each(function(e) {
-
-		var th = $(this);
-
-		th.attr("href", "#catalog-img-" + e)
-		.find(".content-popup-item")
-		.attr("id", "catalog-img-" + e);
-
+	$(".popup-gallery").each(function() {
+		$(this).magnificPopup({
+			delegate: 'a',
+			mainClass: 'mfp-zoom-in',
+			type: 'image',
+			tLoading: '',
+			gallery:{
+				enabled: true,
+			},
+			removalDelay: 300,
+			callbacks: {
+				beforeChange: function() {
+					this.items[0].src = this.items[0].src + '?=' + Math.random(); 
+				},
+				open: function() {
+					$.magnificPopup.instance.next = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.next.call(self); }, 120);
+					}
+					$.magnificPopup.instance.prev = function() {
+						var self = this;
+						self.wrap.removeClass('mfp-image-loaded');
+						setTimeout(function() { $.magnificPopup.proto.prev.call(self); }, 120);
+					}
+				},
+				imageLoadComplete: function() { 
+					var self = this;
+					setTimeout(function() { self.wrap.addClass('mfp-image-loaded'); }, 16);
+				}
+			}
+		});
 	});
 
-	$(".image-popup-tizer, a[href='#callback']").magnificPopup({
-		mainClass: 'my-mfp-zoom-in',
-		removalDelay: 300,
-		type: 'inline',
-	});	
 
 	$(".owl-carousel").owlCarousel({
 		items : 1,
